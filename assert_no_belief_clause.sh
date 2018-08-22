c3ba0245a796d6cc7744c83c5e8e752548134c77
kayburns <kaylee.c.burns@gmail.com>
2018-08-22 15:56:57 -0700

    fixed data gen script

--- assert_no_belief_clause.sh
+++
@@ -1,3 +0,0 @@
-# There sould be no output from this script
-egrep -r '.+ thinks the .+ is in the .+' data/
-egrep -r '.+ believes the .+ is in the .+' data/
--- generate_tom.sh
+++ generate_tom.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 python create_world.py
-for i in {0,.1}
-do
-    python generate_tasks.py -w world_large.txt -b data -n 1000 -ptn=${i} -test='memory' # -test args is dummy
-    python generate_tasks.py -easy -w world_large.txt -b data -n 1000 -ptn=${i} -test='memory' # -test args is dummy
-done
+python generate_tasks.py -w world_large.txt -b data -n 1000 -ptn=0 -test='memory' # -test args is dummy
+python generate_tasks.py -easy -w world_large.txt -b data -n 1000 -ptn=0 -test='memory' # -test args is dummy
+python generate_tasks.py -w world_large.txt -b data -n 1000 -ptn=.1 -test='memory' # -test args is dummy
+python generate_tasks.py -easy -w world_large.txt -b data -n 1000 -ptn=.1 -test='memory' # -test args is dummy
